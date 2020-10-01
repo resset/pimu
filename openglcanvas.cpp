b2854440fc70e098a35a1232cd43261a723fc733
Mateusz Tomaszkiewicz <ressetpublic@gmail.com>
2020-10-01 20:04:51 +0200

    Add simple shape drawing

--- openglcanvas.cpp
+++ openglcanvas.cpp
@@ -18,6 +18,9 @@ OpenGLCanvas::~OpenGLCanvas()
 void OpenGLCanvas::initializeGL()
 {
     initializeOpenGLFunctions();
+
+    glClearColor(0.4f, 0.4f, 0.4f, 0.0f);
+    glEnable(GL_DEPTH_TEST);
 }
 
 void OpenGLCanvas::resizeGL(int w, int h)
@@ -29,6 +32,54 @@ void OpenGLCanvas::resizeGL(int w, int h)
 void OpenGLCanvas::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    glBegin(GL_QUADS);
+
+    // Left
+    glColor3f(1.0f, 0.0f, 0.0f); // Red
+    glVertex3f(-0.5f, -0.5f, 1.0f);
+    glVertex3f(-0.5f, 0.5f, 1.0f);
+    glVertex3f(-0.5f, 0.5f, -1.0f);
+    glVertex3f(-0.5f, -0.5f, -1.0f);
+
+    // Right
+    glColor3f(0.0f, 1.0f, 0.0f); // Green
+    glVertex3f(0.5f, -0.5f, -1.0f);
+    glVertex3f(0.5f, 0.5f, -1.0f);
+    glVertex3f(0.5f, 0.5f, 1.0f);
+    glVertex3f(0.5f, -0.5f, 1.0f);
+
+    // Nose
+    glColor3f(1.0f, 1.0f, 1.0f); // White
+    glVertex3f(-0.5f, -0.5f, -1.0f);
+    glVertex3f(0.5f, -0.5f, -1.0f);
+    glVertex3f(0.5f, 0.5f, -1.0f);
+    glVertex3f(-0.5f, 0.5f, -1.0f);
+
+    // Tail
+    glColor3f(0.0f, 0.0f, 0.0f); // Black
+    glVertex3f(-0.5f, -0.5f, 1.0f);
+    glVertex3f(0.5f, -0.5f, 1.0f);
+    glVertex3f(0.5f, 0.5f, 1.0f);
+    glVertex3f(-0.5f, 0.5f, 1.0f);
+
+    // Top
+    glColor3f(0.2f, 0.2f, 0.2f);
+    glVertex3f(-0.5f, 0.5f, 1.0f);
+    glVertex3f( 0.5f, 0.5f, 1.0f);
+    glVertex3f( 0.5f, 0.5f, -1.0f);
+    glVertex3f(-0.5f, 0.5f, -1.0f);
+
+    // Bottom
+    glColor3f(0.2f, 0.2f, 0.2f);
+    glVertex3f(-0.5f, -0.5f, 1.0f);
+    glVertex3f(-0.5f, -0.5f, -1.0f);
+    glVertex3f( 0.5f, -0.5f, -1.0f);
+    glVertex3f( 0.5f, -0.5f, 1.0f);
+
+    glEnd();
 }
 
 void OpenGLCanvas::setXRotation(int angle)
